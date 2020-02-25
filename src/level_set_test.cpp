5cecff9c71099949f77d6036f5073a495cceb829
gdeskos <gdeskosv@gmail.com>
2020-02-25 01:47:43 -0700

    added timing + MPI init and finalize

--- src/level_set_test.cpp
+++ src/level_set_test.cpp
@@ -1,10 +1,29 @@
-#include<iostream>
+#include <AMReX_PlotFileUtil.H>
+#include <AMReX_ParmParse.H>
+#include <AMReX_Print.H>
 #include<AMReX_Geometry.H>
 #include<AMReX_MultiFab.H>
 #include<AMReX_Array.H>
 
-int main()
+using namespace amrex;
+
+void initialize_fields();
+
+int main(int argc, char* argv[])
 {
-    std::cout<<"Hello from amr-offshore-utils"<<std::endl;
+    amrex::Initialize(argc,argv);
+    
+    initialize_fields();
+
+    amrex::Finalize();
     return 0;
+}
+
+void initialize_fields()
+{
+    Real strt_time = amrex::second();
+    amrex::Print() << "Initialising fields for level-set"<< std::endl;
+    Real stop_time = amrex::second() - strt_time;
+    
+    amrex::Print() << "total time of initialization "<<stop_time<< std::endl;
 }
